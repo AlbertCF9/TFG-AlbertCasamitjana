cbb12950a47f24d906fbcd5bc2539602a07a74cd
AlbertCF9 <albertcasamitjana@gmail.com>
2025-03-09 16:58:22 +0100

    aaa

---
+++ prova.py
@@ -0,0 +1,13 @@
+import pandas
+import ROOT
+print("hola")
+df = ROOT.RDataFrame("TupleOmegac2OmegaPiPiPi_DDL/DecayTree", "../00229334_00000001_1.hyperons.root")
+
+
+pd_df = pandas.DataFrame(df.AsNumpy())#(columns=["Omegac_M"]))
+
+import matplotlib.pyplot as plt
+print(pd_df.keys())
+plt.hist(pd_df["Omegac_M"], bins=100)
+plt.savefig("test_plot.pdf")
+print(pd_df)
