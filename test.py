926e7b53eb8312c1cc8ac5259a7a98a3591c36c5
AlbertCF9 <albertcasamitjana@gmail.com>
2025-03-11 00:29:50 +0100

    Diversos histogrames

--- test.py
+++ test.py
@@ -2,13 +2,37 @@ import ROOT
 import pandas
 import matplotlib.pyplot as plt
 
-
 df = ROOT.RDataFrame("TupleOmegac2OmegaPiPiPi_DDL/DecayTree", "../00229334_00000001_1.hyperons.root")
-print("1")
-pd_df = pandas.DataFrame(df.AsNumpy())#(columns=["Omegac_M"]))
-print("2")
-print(pd_df.keys())
-print("3")
-plt.hist(pd_df["Omegac_M"], bins=100)
-plt.savefig("test_plot2.pdf")
-print(pd_df)
+
+#pd_df = pandas.DataFrame(df.AsNumpy())
+
+#Vertex fit quality omegac
+#pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_OWNPV_CHI2"]))
+#Direction angle omegac
+#pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_PHI"]))
+#Transversal moment omegac
+#pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_PT"]))
+#Omegac mass
+#pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_M"]))
+#Omegac impact parameter
+pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_IPCHI2_OWNPV"]))
+#No entenc el grafic
+
+#Omega  decay Length
+#pd_df = pandas.DataFrame(df.AsNumpy(columns=["Omegac_DTF_Omegaminus_decayLength"]))
+# Aquest no ha sortit
+
+
+#titols = pd_df.iloc[0]
+#titols.to_csv('titols.txt',sep='\t')
+#print(pd_df.keys())
+
+plt.hist(pd_df["Omegac_IPCHI2_OWNPV"], bins=100)
+plt.title("Omegac impact parameter",fontsize=14)
+plt.xlabel("u.a.")
+plt.ylabel("Frequencia")
+#plt.xlim(-25,25) #Rang eix x
+plt.savefig("Impact_parameter_omegac.pdf")
+
+
+#print(pd_df)
