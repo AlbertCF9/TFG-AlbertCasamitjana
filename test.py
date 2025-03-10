314d2d582d19f35e87416af2dfc31975e8f7880a
AlbertCF9 <albertcasamitjana@gmail.com>
2025-03-10 12:00:51 +0100

    Neteja de fitxers

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import ROOT
+import pandas
+import matplotlib.pyplot as plt
+
 
 df = ROOT.RDataFrame("TupleOmegac2OmegaPiPiPi_DDL/DecayTree", "../00229334_00000001_1.hyperons.root")
-
-import pandas
+print("1")
 pd_df = pandas.DataFrame(df.AsNumpy())#(columns=["Omegac_M"]))
-
-import matplotlib.pyplot as plt
+print("2")
 print(pd_df.keys())
+print("3")
 plt.hist(pd_df["Omegac_M"], bins=100)
-plt.savefig("test_plot.pdf")
+plt.savefig("test_plot2.pdf")
 print(pd_df)
-
-
