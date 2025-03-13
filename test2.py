efae1ea43fa4c8b4f0970ecdffcf06d9e44f1729
AlbertCF9 <albertcasamitjana@gmail.com>
2025-03-13 15:27:31 +0100

    Molts canvis

--- test2.py
+++ test2.py
@@ -7,9 +7,7 @@ import matplotlib.pyplot as plt
 for i in ["LLL","DDL","DDD"]:
     df = ROOT.RDataFrame(f"TupleOmegac2OmegaPiPiPi_{i}/DecayTree", "../00229334_00000001_1.hyperons.root")
 
-    llista = ["Omegac_M","Omegac_OWNPV_CHI2","Omegac_PHI","Omegac_PT","Omegac_IP_OWNPV","Omega_M"]
-
-    #pd_df = pandas.DataFrame(df.AsNumpy())
+    llista = [f"{particle}_P{component}" for particle in ["L0","K"] for component in ["E","X","Y","Z"]]
 
     pd_df = pandas.DataFrame(df.AsNumpy(columns=llista))
 
@@ -18,6 +16,5 @@ for i in ["LLL","DDL","DDD"]:
         plt.title(j+i,fontsize=14)
         plt.xlabel("x")
         plt.ylabel("Frequencia")
-        #plt.xlim(-25,25) #Rang eix x
-        plt.savefig(f"{j}{i}.pdf")
+        plt.savefig(f"{j}-{i}.pdf")
         plt.clf()
